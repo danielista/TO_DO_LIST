762ab70ec6abc992cb081281149c1a7d5defc4aa
danielista <61905525+danielista@users.noreply.github.com>
2021-04-20 10:55:43 +0200

    all methods DONE

--- src/main/java/sk/martinek/task/mongodb/MongoImplementation.java
+++ src/main/java/sk/martinek/task/mongodb/MongoImplementation.java
@@ -1,17 +1,26 @@
 package sk.martinek.task.mongodb;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.bson.Document;
 import org.bson.types.ObjectId;
+import org.json.simple.JSONObject;
 import sk.martinek.task.collection.Task;
+import org.json.simple.parser.ParseException;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+
 
 public class MongoImplementation implements Mongosdatabase {
 
@@ -54,15 +63,12 @@ public class MongoImplementation implements Mongosdatabase {
     }
 
     public List<Task> getAllTasks() {
-
-
         try {  MongoClient mongoClient = new MongoClient();
             MongoDatabase database = mongoClient.getDatabase("testovanie");
             MongoCollection<Document> collec = database.getCollection("tasks");
 
             FindIterable<Document> cursor = collec.find();
             List<Task> list = new ArrayList<Task>();
-
 
             for (Document document : cursor) {
                 System.out.println(document.toString());
@@ -92,23 +98,168 @@ public class MongoImplementation implements Mongosdatabase {
         return null;
     }
 
+    //db.collec.find({done: "true"})
     public List<Task> getAllTasks(boolean done) {
+        try {  MongoClient mongoClient = new MongoClient();
+            MongoDatabase database = mongoClient.getDatabase("testovanie");
+            MongoCollection<Document> collec = database.getCollection("tasks");
+
+
+            FindIterable<Document> cursor = (FindIterable<Document>) collec.find(eq("done", done));
+
+            List<Task> list = new ArrayList<Task>();
+
+            for (Document document : cursor) {
+                System.out.println(document.toString());
+                String name =  document.getString("name");
+                int priority = document.getInteger("priority");
+                done = document.getBoolean("done");
+                Date date = (Date) document.getDate("date");
+                ObjectId id = document.getObjectId("_id");
+                Task task;
+
+                if(document.containsKey("price")){
+                    double price = document.getDouble("price");
+                    task = new Task(name,priority,done,date,price);
+                }else{
+                    task = new Task(name,priority,done,date);
+
+                }
+
+                task.setId(id);
+                list.add(task);
+            }
+            return list;
+            //while (cursor.cursor().hasNext())
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public List<Task> getAllTasksByPriority(int priority) {
+    //db.collec.find({priority: "priorityNumber"})
+    public List<Task> getAllTasksByPriority(int priorityNumber) {
+        try {  MongoClient mongoClient = new MongoClient();
+            MongoDatabase database = mongoClient.getDatabase("testovanie");
+            MongoCollection<Document> collec = database.getCollection("tasks");
+
+
+            FindIterable<Document> cursor = (FindIterable<Document>) collec.find(eq("priority", priorityNumber));
+
+            List<Task> list = new ArrayList<Task>();
+
+            for (Document document : cursor) {
+                System.out.println(document.toString());
+                String name =  document.getString("name");
+                int priority = document.getInteger("priority");
+                Boolean done = document.getBoolean("done");
+                Date date = (Date) document.getDate("date");
+                ObjectId id = document.getObjectId("_id");
+                Task task;
+
+                if(document.containsKey("price")){
+                    double price = document.getDouble("price");
+                    task = new Task(name,priority,done,date,price);
+                }else{
+                    task = new Task(name,priority,done,date);
+
+                }
+
+                task.setId(id);
+                list.add(task);
+            }
+            return list;
+            //while (cursor.cursor().hasNext())
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
+    //db.collec.find({name: "name"})
     public List<Task> getTaskByName(String name) {
+
+        try {  MongoClient mongoClient = new MongoClient();
+            MongoDatabase database = mongoClient.getDatabase("testovanie");
+            MongoCollection<Document> collec = database.getCollection("tasks");
+
+
+            FindIterable<Document> cursor = (FindIterable<Document>) collec.find(eq("name", name));
+
+            List<Task> list = new ArrayList<Task>();
+
+            for (Document document : cursor) {
+                System.out.println(document.toString());
+                name =  document.getString("name");
+                int priority = document.getInteger("priority");
+                Boolean done = document.getBoolean("done");
+                Date date = (Date) document.getDate("date");
+                ObjectId id = document.getObjectId("_id");
+                Task task;
+
+                if(document.containsKey("price")){
+                    double price = document.getDouble("price");
+                    task = new Task(name,priority,done,date,price);
+                }else{
+                    task = new Task(name,priority,done,date);
+
+                }
+
+                task.setId(id);
+                list.add(task);
+            }
+            return list;
+            //while (cursor.cursor().hasNext())
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
+    //db.collec.find({date: "date"})
     public List<Task> getTaskByDate(Date date) {
+        try {  MongoClient mongoClient = new MongoClient();
+            MongoDatabase database = mongoClient.getDatabase("testovanie");
+            MongoCollection<Document> collec = database.getCollection("tasks");
+
+
+            FindIterable<Document> cursor = collec.find(eq("date", date));
+
+            List<Task> list = new ArrayList<Task>();
+
+            for (Document document : cursor) {
+                System.out.println(document.toString());
+                String name =  document.getString("name");
+                int priority = document.getInteger("priority");
+                Boolean done = document.getBoolean("done");
+                date = (Date) document.getDate("date");
+                ObjectId id = document.getObjectId("_id");
+                Task task;
+
+                if(document.containsKey("price")){
+                    double price = document.getDouble("price");
+                    task = new Task(name,priority,done,date,price);
+                }else{
+                    task = new Task(name,priority,done,date);
+
+                }
+
+                task.setId(id);
+                list.add(task);
+            }
+            return list;
+            //while (cursor.cursor().hasNext())
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     public void deleteAllDoneTasks() {
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase database = mongoClient.getDatabase("testovanie");
+        MongoCollection<Document> collec = database.getCollection("tasks");
 
+        collec.deleteMany(new Document().append("done", true));
     }
 }
